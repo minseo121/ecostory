0646b0de6c40943e58251924ee74f1617ae653b3
Koohyewon <khw6746@naver.com>
2024-02-05 22:31:17 +0900

    update Post Modal

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,17 @@
-import react from 'react';
+import {useState} from 'react';
 import Header from '../components/Header/Header_AfterLogin';
+import Modal from '../components/PostModal';
 
 function Profile() {
+    const [modalOpen, setModal] = useState(false);
+
     return (
         <div>
 
             <Header/>     
 
-            <div className='bg-white flex h-screen mt-16'>
-                <div className='flex-1'>
+            <div className='bg-white h-screen mt-16'>
+                <div className='flex flex-col justify-center items-center'>
                     <div className='h-[150px] w-[150px] max-h-[150px] max-w-[150px] mx-auto rounded-full border-4 border-[#A9D6BE]'></div>
                     <div className='flex justify-center mt-[15px] '>
                         <div className='text-[#589B7F] text-xl mr-1.5'>구혜원</div>
@@ -17,8 +20,13 @@ function Profile() {
                         </div>
                     </div>
                 </div>
-            </div> 
 
+                <div className=''>
+                    <button onClick={()=>{setModal(true)}}>모달창 열기</button>
+                    {modalOpen === true ? <Modal modalClose={setModal} /> : null}
+                </div>
+
+            </div> 
         </div>
     );
 }
