51f7277eabfd05e1f6f38d455b82286d47a9b674
Koohyewon <khw6746@naver.com>
2024-02-27 11:24:27 +0900

    update checkbox

--- src/components/Sidebar/Sidebar_AfterLogin.js
+++ src/components/Sidebar/Sidebar_AfterLogin.js
@@ -12,7 +12,7 @@ function CheckList() {
             <div className="inline-flex items-center ">
                 <label className="relative flex items-center py-2 mr-2 rounded-full cursor-pointer" htmlFor="customStyle">
                     <input type="checkbox"
-                    className="before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-full border-2 border-[#C3E0D1] bg-white transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-[#C3E0D1] checked:bg-[#C3E0D1] checked:before:bg-gray-900 hover:scale-105 hover:before:opacity-0"
+                    className="before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-full border-2 border-[#C3E0D1] bg-white transition-all checked:border-[#C3E0D1] checked:bg-[#C3E0D1] hover:scale-105"
                     //checked={true}
                     />
                     <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
