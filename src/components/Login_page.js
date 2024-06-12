6d7719febbf731c16385795ef047ff7e604035cf
minseo121 <kittse1217@gmail.com>
2024-06-12 17:33:11 +0900

    feat : signup 기능 구현 완료, login 기능 구현 중

--- src/components/Login_page.js
+++ src/components/Login_page.js
@@ -1,45 +1,77 @@
-import React, {useEffect, useState} from "react";
+import React, { useState } from "react";
 import '../css/login.css';
-import axios from "axios"; 
+import axios from "axios";
 
 function Login_page() {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
+    const [loginSuccess, setLoginSuccess] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    return(
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://13.209.53.13:8000/user/login', 
+                {
+                    userid: id,
+                    password: password
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+            if (response.status === 200) {
+                setLoginSuccess(true);
+                setErrorMessage('');
+            } else {
+                setLoginSuccess(false);
+                setErrorMessage('로그인에 실패했습니다. 다시 시도해주세요.');
+            }
+        } catch (error) {
+            setLoginSuccess(false);
+            setErrorMessage('로그인에 실패했습니다. 다시 시도해주세요.');
+            console.log("먼 에러임 : ",error)
+        }
+    };
+
+    return (
         <>
-        <div className="Login bg-[#D3E7DD]">
-            <div className="container sm:w-3/4 mx-auto">
-                <div className="login_content w-full bg-white text-[#589B7F] text-lg h-screen flex flex-col justify-center items-center pt-36">
-                    <div className="mb-10 w-9/14">
-                        <div className="login_title mb-5 mr-24">
-                            <p className="login_title1 text-2xl mb-1">로그인</p>
-                            <p className="text-[#7ab89d] text-sm">사소한 실천이 나아가서 큰 변화가 오게 함께 해주세에코</p>
+            <div className="Login bg-[#D3E7DD]">
+                <div className="container sm:w-3/4 mx-auto">
+                    <div className="login_content w-full bg-white text-[#589B7F] text-lg h-screen flex flex-col justify-center items-center pt-36">
+                        <div className="mb-10 w-9/14">
+                            <div className="login_title mb-5 mr-24">
+                                <p className="login_title1 text-2xl mb-1">로그인</p>
+                                <p className="text-[#7ab89d] text-sm">사소한 실천이 나아가서 큰 변화가 오게 함께 해주세에코</p>
+                            </div>
+                            <hr />
+                            <form className="login_form mt-8 mx-16" onSubmit={handleSubmit}>
+                                <div className="mb-3">
+                                    <p className="pl-1">아이디</p>
+                                    <input type="text" placeholder="아이디" className="id_input border-4 border-[#7bb49c56] w-full my-2 p-3 rounded-xl" onChange={(e) => setId(e.target.value)} />
+                                </div>
+                                <div>
+                                    <p className="pl-1">비밀번호</p>
+                                    <input type="password" placeholder="비밀번호" className="pw_input border-4 border-[#7bb49c56] w-full my-2 p-3 rounded-xl" onChange={(e) => setPassword(e.target.value)} />
+                                </div>
+                                <div className="flex items-center mt-3 mb-10">
+                                    <input type="checkbox"></input>
+                                    <label className="ml-3 text-sm">로그인 상태 유지하기</label>
+                                </div>
+                                <div className="mt-5 flex justify-center text-[#ffff] mb-10">
+                                    <button type="submit" className="btn_input bg-[#7BB49C] p-4 px-14 rounded-xl text-lg">로그인</button>
+                                </div>
+                                {loginSuccess && <p className="text-green-500">로그인 성공했다</p>}
+                                {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+                            </form>
                         </div>
-                        <hr/>
-                        <form className="login_form mt-8 mx-16">
-                            <div className="mb-3">
-                                <p className="pl-1">아이디</p>
-                                <input type="text" placeholder="아이디" className="id_input border-4 border-[#7bb49c56] w-full my-2 p-3 rounded-xl" />
-                            </div>
-                            <div>
-                                <p className="pl-1">비밀번호</p>
-                                <input type="password" placeholder="비밀번호" className="pw_input border-4 border-[#7bb49c56] w-full my-2 p-3 rounded-xl" />
-                            </div>
-                            <div className="flex items-center mt-3 mb-10">
-                                <input type="checkbox"></input>
-                                <label className="ml-3 text-sm">로그인 상태 유지하기</label>
-                            </div>
-                            <div className="mt-5 flex justify-center text-[#ffff] mb-10">
-                                <input type="button" value="로그인" className="btn_input bg-[#7BB49C] p-4 px-14 rounded-xl text-lg"/>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
-        </div>
         </>
-    )
+    );
 }
 
 export default Login_page;
---
+++ src/components/Modal/SuccessModal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SuccessModal = () => {
+    return (
+        <div>
+            <div className="modal fixed w-full h-full top-0 left-0 flex items-center justify-center">
+                <div className="modal-container bg-white w-1/3 rounded shadow-lg z-100 overflow-y-auto">
+
+                    <div className="modal-content py-16 text-center px-6">
+                        <div className="modal-header">
+                            <h3 className="text-2xl font-bold text-[#589B7F]">가입 성공</h3>
+                        </div>
+
+                        <div className="modal-footer mt-14">
+                            <Link to="/login" className="btn bg-[#7BB49C] text-white p-6 px-10 rounded-xl text-lg hover:bg-[#589B7F] hover:cursor-pointer">로그인 하러가기</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SuccessModal;
